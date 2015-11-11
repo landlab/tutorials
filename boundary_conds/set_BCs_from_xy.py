2130a74c6ecd3d189897783a9a3d67f1ba9e0b73
Jenny Knuth <inkboco@gmail.com>
2015-11-10 21:16:13 -0700

    add boundary_conds

---
+++ boundary_conds/set_BCs_from_xy.py
@@ -0,0 +1,25 @@
+
+# Modify the boundary conditions of an interior rectangle in the grid
+
+from landlab import RasterModelGrid, CLOSED_BOUNDARY
+import numpy as np
+from landlab.plot.imshow import imshow_node_grid
+from matplotlib.pyplot import show
+get_ipython().magic(u'matplotlib inline')
+
+mg = RasterModelGrid(10,10,1.)
+
+min_x = 2.5
+max_x = 5.
+min_y = 3.5
+max_y = 7.5
+
+x_condition = np.logical_and(mg.node_x < max_x, mg.node_x > min_x)
+y_condition = np.logical_and(mg.node_y < max_y, mg.node_y > min_y)
+my_nodes = np.logical_and(x_condition, y_condition)
+
+mg.status_at_node[my_nodes] = CLOSED_BOUNDARY
+
+z = mg.add_zeros('node', 'topographic__elevation')
+
+imshow_node_grid(mg, z)
