9ba928957c0558f0a639b869c2437ca7d76d5163
saisiddu <saisiddu@uw.edu>
2016-07-21 00:20:09 -0700

    Minor_adjustments
    - Adjusted DEM tutorial to follow PEP 8 as much as possible.
    - Added a command to print years elapsed at regular intervals.

--- ecohydrology/cellular_automaton_vegetation_DEM/ca_veg_dem_py_file.py
+++ ecohydrology/cellular_automaton_vegetation_DEM/ca_veg_dem_py_file.py
@@ -3,7 +3,7 @@
 Created on Wed Jul 20 2016
 
 This tutorial is on:
-landlab/tutorials/ecohydrology/cellular_automaton_vegetation_flat_surface.ipynb
+landlab/tutorials/ecohydrology/cellular_automaton_vegetation_DEM.ipynb
 
 Creating a (.py) version of the same.
 
@@ -12,20 +12,21 @@ Creating a (.py) version of the same.
 
 import time
 import numpy as np
+from landlab.io import read_esri_ascii
 from landlab import RasterModelGrid as rmg
-from Ecohyd_functions_flat import (txt_data_dict, Initialize_, Empty_arrays,
-                                   Create_PET_lookup, Save_, Plot_)
+from Ecohyd_functions_DEM import (txt_data_dict, Initialize_, Empty_arrays,
+                                  Create_PET_lookup, Save_, Plot_)
 
-grid1 = rmg((100, 100), spacing=(5., 5.))
-grid = rmg((5, 4), spacing=(5., 5.))
+(grid, elevation) = read_esri_ascii('DEM_10m.asc')    # Read the DEM
+grid1 = rmg((5, 4), spacing=(5., 5.))                 # Representative grid
 
 InputFile = 'Inputs_Vegetation_CA.txt'
-data = txt_data_dict(InputFile)  # Create dictionary that holds the inputs
+data = txt_data_dict(InputFile)  # Creates dictionary that holds the inputs
 
-PD_D, PD_W, Rad, PET_Tree, PET_Shrub, PET_Grass, SM, VEG, vegca = Initialize_(
-            data, grid, grid1)
+PD_D, PD_W, Rad, Rad_PET, PET_Tree, PET_Shrub, PET_Grass, SM, VEG, vegca = (
+                Initialize_(data, grid, grid1, elevation))
 
-n_years = 1200      # Approx number of years for model to run
+n_years = 50       # Approx number of years for model to run
 # Calculate approximate number of storms per year
 fraction_wet = (data['doy__end_of_monsoon']-data['doy__start_of_monsoon'])/365.
 fraction_dry = 1 - fraction_wet
@@ -35,50 +36,53 @@ no_of_storms_dry = (8760 * (fraction_dry)/(data['mean_interstorm_dry'] +
                     data['mean_storm_dry']))
 n = int(n_years * (no_of_storms_wet + no_of_storms_dry))
 
-P, Tb, Tr, Time, VegType, PET_, Rad_Factor, EP30, PET_threshold = Empty_arrays(
-                n, grid, grid1)
+P, Tb, Tr, Time, VegType, PET_, Rad_Factor, EP30, PET_threshold = (
+                Empty_arrays(n, n_years, grid, grid1))
 
 Create_PET_lookup(Rad, PET_Tree, PET_Shrub, PET_Grass,  PET_, Rad_Factor,
-                  EP30, grid)
+                  EP30, Rad_PET, grid)
 
 # # Represent current time in years
 current_time = 0            # Start from first day of Jan
 
-# Keep track of run time for simulation - optional
+# Keep track of run time for simulation—optional
 Start_time = time.clock()     # Recording time taken for simulation
 
-# declaring few variables that will be used in the storm loop
+# declaring few variables that will be used in storm loop
 time_check = 0.     # Buffer to store current_time at previous storm
 yrs = 0             # Keep track of number of years passed
 WS = 0.             # Buffer for Water Stress
-Tg = 270        # Growing season in days
+Tg = 365        # Growing season in days
 
 # # Run storm Loop
 for i in range(0, n):
-    # Update objects
-
+    if i % 5 == 0:
+        print 'Elapsed time = ', i, ' years'
+    # # Update objects
     # Calculate Day of Year (DOY)
     Julian = np.int(np.floor((current_time - np.floor(current_time)) * 365.))
-
     # Generate seasonal storms
     # for Dry season
     if Julian < data['doy__start_of_monsoon'] or Julian > data[
-                        'doy__end_of_monsoon']:
+                                'doy__end_of_monsoon']:
         PD_D.update()
-        P[i] = PD_D.storm_depth
-        Tr[i] = PD_D.storm_duration
-        Tb[i] = PD_D.interstorm_duration
-    # Wet Season - Jul to Sep - NA Monsoon
+        P[i] = PD_D.get_storm_depth()
+        Tr[i] = PD_D.get_precipitation_event_duration()
+        Tb[i] = PD_D.get_interstorm_event_duration()
+    # Wet Season—Jul to Sep—NA Monsoon
     else:
         PD_W.update()
-        P[i] = PD_W.storm_depth
-        Tr[i] = PD_W.storm_duration
-        Tb[i] = PD_W.interstorm_duration
+        P[i] = PD_W.get_storm_depth()
+        Tr[i] = PD_W.get_precipitation_event_duration()
+        Tb[i] = PD_W.get_interstorm_event_duration()
 
     # Spatially distribute PET and its 30-day-mean (analogous to degree day)
-    grid['cell']['surface__potential_evapotranspiration_rate'] = PET_[Julian]
-    grid['cell']['surface__potential_evapotranspiration_30day_mean'] = EP30[
-                Julian]
+    grid['cell']['surface__potential_evapotranspiration_rate'] = (
+                (np.choose(grid['cell']['vegetation__plant_functional_type'],
+                           PET_[Julian])) * Rad_Factor[Julian])
+    grid['cell']['surface__potential_evapotranspiration_30day_mean'] = (
+                (np.choose(grid['cell']['vegetation__plant_functional_type'],
+                           EP30[Julian])) * Rad_Factor[Julian])
 
     # Assign spatial rainfall data
     grid['cell']['rainfall__daily'] = P[i] * np.ones(grid.number_of_cells)
@@ -96,7 +100,7 @@ for i in range(0, n):
             # 0 corresponds to ETThresholddown (end growing season)
 
     # Update vegetation component
-    VEG.update(PETThreshold_switch=PET_threshold, Tb=Tb[i], Tr=Tr[i])
+    VEG.update(PETthreshold_switch=PET_threshold, Tb=Tb[i], Tr=Tr[i])
 
     # Update yearly cumulative water stress data
     WS += (grid['cell']['vegetation__water_stress'])*Tb[i]/24.
@@ -104,26 +108,26 @@ for i in range(0, n):
     # Record time (optional)
     Time[i] = current_time
 
-    # Update spatial PFTs with Cellular Automata rules
+    # Cellular Automata
     if (current_time - time_check) >= 1.:
-        if yrs % 100 == 0:
+        if yrs % 5 == 0:
             print 'Elapsed time = ', yrs, ' years'
-        VegType[yrs] = grid1['cell']['vegetation__plant_functional_type']
-        WS_ = np.choose(VegType[yrs], WS)
-        grid1['cell']['vegetation__cumulative_water_stress'] = WS_/Tg
+        VegType[yrs] = grid['cell']['vegetation__plant_functional_type']
+        grid['cell']['vegetation__cumulative_water_stress'] = WS/Tg
         vegca.update()
+        SM.initialize()
+        VEG.initialize()
         time_check = current_time
         WS = 0
         yrs += 1
-
-VegType[yrs] = grid1['cell']['vegetation__plant_functional_type']
+VegType[yrs] = grid['cell']['vegetation__plant_functional_type']
 
 Final_time = time.clock()
 Time_Consumed = (Final_time - Start_time)/60.    # in minutes
 print 'Time_consumed = ', Time_Consumed, ' minutes'
 
 # # Saving
-# sim = 'Sim_26Jul16_'
+sim = 'VegCA_DEM_26Jul16_'
 # Save_(sim, Tb, Tr, P, VegType, yrs, Time_Consumed, Time)
 
-Plot_(grid1, VegType, yrs, yr_step=100)
+Plot_(grid, VegType, yrs, yr_step=10)
