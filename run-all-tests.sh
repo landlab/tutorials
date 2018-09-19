7fc5d69f8547ec3899e43255b3cb7e24befa31ad
kbarnhart <katy.barnhart@gmail.com>
2018-09-19 09:01:34 -0600

    removing exclusion of flow direction

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-EXCLUDES="--exclude=*intro* --exclude=*flow_direction*"
+EXCLUDES="--exclude=*intro*" # krb note: as best as I can tell this is only supposed to skip the tutorial python_intro/Python_intro.ipynb
 
 ./run_notebook.py find . | ./run_notebook.py run --file=- $EXCLUDES
